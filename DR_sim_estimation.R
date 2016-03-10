5c832cf52c7fcecd0d600704762782301705199e
Bradley Saul <bradley@BigScreen.local>
2016-03-10 10:21:54 -0500

    sets up estimation for getting results from all sims

--- DR_sim_estimation.R
+++ DR_sim_estimation.R
@@ -14,32 +14,29 @@ library(sandwichShop)
 
 alphas <- c(0.1, 0.5, 0.9)
 
-DRresults <- DRsims %>% 
-   filter(simID <= 2) %>%
-  {plyr::dlply(., plyr::.(simID), function(sim){
-    this_sim <- dr_ipw(formula_outcome = Y ~ Z1 + Z2 + Z3 + Z4 + A + fA + A*Z1 + fA*Z2,
-           formula_interference = Y | A ~ -1 + Z1 + Z2 + Z3 + Z4 +(1|group) | group,
-           method_outcome  = geepack::geeglm,
-           method_opts_outcome = list(id = quote(group), family = gaussian),
-           method_opts_interference = list(allocations = alphas,
-                                           method = 'simple', 
-                                           runSilent = T),
-           dr_term2_function = dr_term2,
-           data = sim)
-    dr_ipw_estimate(this_sim) %>%
-      mutate_(simID = ~ sim$simID[1])
-  })} %>%
-  bind_rows()
+tru_outcome <- Y ~ Z1 + Z2 + Z3 + Z4 + A + fA + A*Z1 + fA*Z2
+tru_propen  <- Y | A ~ -1 + Z1 + Z2 + Z3 + Z4 + (1|group) | group
+mis_outcome <- Y ~ X1 + X2 + X3 + X4 + A + fA + A*X1 + fA*X2
+mis_propen  <- Y | A ~ -1 + X1 + X2 + X3 + X4 + (1|group) | group  
 
-DRresults
-# 
-# temp <- dr_ipw(formula_outcome = Y ~ A + X1,
-#                formula_interference = Y | A ~ X1 + (1|group) | group,
-#                method_outcome  = geepack::geeglm,
-#                method_opts_outcome = list(id = quote(group), family = gaussian),
-#                method_opts_interference = list(allocations = alphas,
-#                                                method = 'simple', 
-#                                                runSilent = T),
-#                dr_term2_function = dr_term2,
-#                data = DRsims %>% filter(simID == 2))
-# rm(temp)
+# Outcome_Propensity
+tru_tru <- dr_results(DRsims, tru_outcome, tru_propen)
+mis_tru <- dr_results(DRsims, mis_outcome, tru_propen)
+tru_mis <- dr_results(DRsims, tru_outcome, mis_propen)
+mis_mis <- dr_results(DRsims, mis_outcome, mis_propen)
+
+## True values
+2+0.5*1+6*(alphas*3/4)
+
+
+# inter_size=(n_i-1)
+# bar_y_alpha_1=2+0.5*alpha_1+6*(alpha_1*inter_size/n_i)*inter
+# bar_y1_alpha_1=2+0.5*1+6*(alpha_1*inter_size/n_i)*inter
+# bar_y0_alpha_1=2+0.5*0+6*(alpha_1*inter_size/n_i)*inter
+
+# tru_tru %>%
+#   group_by(type, alpha1) %>%
+#   summarise(median = median(estimate),
+#             mean   = mean(estimate))
+  
+  
