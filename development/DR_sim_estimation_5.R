7192f67cced1cbbcde28d4c0eff8538b3187ac04
Bradley Saul <bradleysaul@gmail.com>
2016-05-01 21:31:30 -0400

    corrects treatment formulas; sets up tru_tru in preparation for simulations

--- development/DR_sim_estimation_5.R
+++ development/DR_sim_estimation_5.R
@@ -15,21 +15,32 @@ library(sandwichShop)
 library(doMC)
 registerDoMC(4)
 
-source(file = 'DR_functions_5.R')
+# source(file = 'DR_functions_5.R')
 
 alphas <- c(0.1, 0.5, 0.9)
 
 tru_outcome <- Y ~ Z1 + Z2 + Z3 + Z4 + A + fA + A*Z1 + fA*Z2
-tru_treatment  <- A ~ -1 + Z1 + Z2 + Z3 + Z4 + (1|group) 
+tru_treatment  <- A ~ Z1 + Z2 + Z3 + Z4 + (1|group) 
 mis_outcome <- Y ~ X1 + X2 + X3 + X4 + A + fA + A*X1 + fA*X2
-mis_treatment  <- A ~ -1 + X1 + X2 + X3 + X4 + (1|group)
+mis_treatment  <- A ~ X1 + X2 + X3 + X4 + (1|group)
 
-DRsims <- sims_500x_m500_n4 %>% filter(simID < 3)
+DRsims <- sims_500x_m500_n4  %>%  filter(simID < 3)
+
+target_a <- 1
 
 truth <- data.frame(alpha = alphas, a = target_a, truth = 2+0.5*target_a+6*(alphas*3/4))
 
 ### Update to write results to results folder.
 # add a comment to each results file
-temp <- estimate_sims(DRsims, tru_treatment, tru_outcome)
+# temp <- estimate_sims(DRsims, tru_treatment, tru_outcome)
+
+tru_tru <- estimate_sims(DRsims, tru_treatment, tru_outcome) %>%
+  left_join(truth, by = c('alpha', 'a')) %>%
+  mutate(conf.low  = estimate - 1.96 * std.error,
+         conf.high = estimate + 1.96 * std.error, 
+         bias      = estimate - truth,
+         covered   = truth > conf.low & truth < conf.high * 1)
+
+save(tru_tru, file = paste0('results/tru_tru_', Sys.Date(), '.rda'))
 
 
