04e0152573863723303d55c90b71123d27c286c9
Bradley Saul <bradley@BigScreen.local>
2016-05-03 07:30:43 -0400

    compares std.error from inferference to current code

--- development/experiments/ex03.R
+++ development/experiments/ex03.R
@@ -4,27 +4,38 @@
 #    Date: 2016-05-1
 # Purpose: comparing the speed of inferference to DR_functions_5
 #------------------------------------------------------------------------------#
-
+library(inferference)
 library(lme4)
 library(dplyr)
 library(geepack)
 library(magrittr)
 library(sandwich)
 library(sandwichShop)
-library(lineprof)
+
+DRsims <- sims_500x_m500_n4 %>% filter(simID < 2)
+tru_outcome <- Y ~ Z1 + Z2 + Z3 + Z4 + A + fA + A*Z1 + fA*Z2
+tru_treatment  <- A ~ Z1 + Z2 + Z3 + Z4 + (1|group) 
 
 # This isn't exactly a fair comparison since interference computes all causal 
 # contrasts but it should give some sense in comparing the key functions
 
-system.time(estimation(tru_treatment, tru_outcome, filter(DRsims, simID == 1),
+system.time(t1 <- estimation(tru_treatment, tru_outcome, filter(DRsims, simID == 1),
                                allocations = c(.1, .5, .9), target_a = 1))
 
 # user  system elapsed 
 # 81.953   0.497  82.723 
 
-system.time(interference(Y | A ~ -1 + Z1 + Z2 + Z3 + Z4 + (1|group) | group,
+
+
+system.time(t2 <- interference(Y | A ~ Z1 + Z2 + Z3 + Z4 + (1|group) | group,
                          data = filter(DRsims, simID == 1),
                          allocations = c(.1, .5, .9),
                          method = 'simple'))
+
+
 # user  system elapsed 
 # 87.672   0.683  88.718 
+
+# compare esimates for ipw
+t1 %>% filter(estimator_type == 'ipw')
+t2$estimates %>% filter(effect == 'outcome', trt1 == 1) %>% select(alpha1, trt1, estimate, std.error)
