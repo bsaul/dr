9f62c01ea2d1db6813c7e89b41134f2de354e7a1
Bradley Saul <bradley@BigScreen.local>
2016-05-03 07:29:14 -0400

    set up of a simulation logger

---
+++ sim_logger.R
@@ -0,0 +1,36 @@
+#------------------------------------------------------------------------------#
+#   TITLE: Simulation Logger
+#  AUTHOR: Bradley Saul
+#    DATE: 5/02/16
+# PURPOSE:
+#------------------------------------------------------------------------------#
+
+ts <- Sys.time()
+estimation_file <- 'DR_sim_estimation_5'
+experimentID <- 'X001'
+
+simdir <- 'logs/'
+filename <- paste0(ts, '_', experimentID, '.log')
+fn <- paste0(simdir, filename)
+
+sink(fn, append = TRUE, type = 'output')
+# sink(fn, append = TRUE, type = 'message')
+
+timestamp(prefix = "##------ Begin: ")
+
+# Notes
+
+# Create similuated dataset
+source('R/create_sims.R', echo = T, max.deparse.length = 5000)
+
+# Load necessary functions
+source('R/functions.R', echo = T, max.deparse.length = 5000)
+
+# Run esimtations
+source('development/DR_sim_estimation_5.R', echo = T, max.deparse.length = 5000)
+
+timestamp(prefix = "##------ End: ")
+
+sink()
+
+rm(ts, simdir, filename, fn)
