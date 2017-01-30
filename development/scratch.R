668478946b3e37984f56a9ddf4557d62f589f719
Bradley Saul <bradleysaul@gmail.com>
2017-01-30 11:15:32 -0500

    scratchy scratch

--- development/scratch.R
+++ development/scratch.R
@@ -1,25 +1,31 @@
 ## 
 library(inferference)
 library(geex)
+library(dplyr)
 ##### 
 
-vaccinesim
+vaccinesim <- vaccinesim %>%
+  group_by(group) %>%
+  mutate(fA = mean(A))
 
 mm <- interference(Y | A ~ X1 + (1|group) | group,
                    allocations = c(.3, .6),
                    data = vaccinesim)
 pmodel <- mm$models$propensity_model
-omodel <- glm(Y ~ X1, data = vaccinesim)
+omodel <- geepack::geeglm(Y ~ A + fA + X1, data = vaccinesim, 
+                          id = group,
+                          family = binomial(link = 'logit'))
+
+
 theta_t <- unlist(lme4::getME(pmodel, c('beta', 'theta')))
+theta_o <- coef(omodel)
 
-
-mylist <-  append(list(eeFUN = ipw_eefun, splitdt = split(vaccinesim, vaccinesim$group)), 
+mylist <-  append(list(eeFUN = lan_eefun, 
+                       splitdt = split(vaccinesim, vaccinesim$group)), 
                   list(ee_args = list(alpha1 = .3, alpha2 = .6, a1 = 0, a2 = 0)))
 
-str(mylist)
-ff <- ipw_eefun(mylist$splitdt[[1]], t_model = pmodel, o_model = omodel)
-ff(theta = c(theta_t, .5, .5), alpha1 = .5, alpha2 = .5, a1 = 0, a2 = 1)
-
+ff <- lan_eefun(mylist$splitdt[[1]], t_model = pmodel, o_model = omodel)
+ff(theta = c(theta_t, theta_o, .5, .5, .5, .5), alpha1 = .5, alpha2 = .5, a1 = 0, a2 = 1)
 
 mats <- compute_matrices(mylist,  
                  theta   = c(theta_t, .5, .5),
@@ -29,16 +35,13 @@ mats <- compute_matrices(mylist,
 
 sig <- compute_sigma(mats$A, mats$B)
 
-sqrt(diag(sig))
-
-summary(pmodel)
 
 ptm <- proc.time()
 test <- geex::estimate_equations(
-  eeFUN   = ipw_eefun,
+  eeFUN   = lan_eefun,
   data    = vaccinesim,
   units   = 'group',
-  roots   = c(theta_t, .5, .5),
+  roots   = c(theta_t, theta_o, .5, .5, .5, .5),
   numDeriv_options = list(method = 'simple'),
   t_model = pmodel,
   o_model = omodel,
