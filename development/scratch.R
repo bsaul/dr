0d3047cc09199e8c5ab65f0c54ad4d7a2d33e8a5
Bradley Saul <bradleysaul@gmail.com>
2017-02-06 10:08:29 -0500

    scratchy scratch

--- development/scratch.R
+++ development/scratch.R
@@ -4,45 +4,45 @@ library(geex)
 library(dplyr)
 ##### 
 
-temp <- simdt$sims[[1]] %>%
-  group_by(group) %>%
-  mutate(fA = mean(A))
+temp1 <- simdt$sims[[1]] %>%
+  group_by_(~group) %>%
+  mutate_(fA = ~mean(A))
 
-# mm <- interference(y | A | B ~ X1 + X2 + (1|group) | group,
-#                    allocations = c(.3, .6),
-#                    data = simdt$sims[[1]])
-tmodel <-  tmodel <- lme4::glmer(B ~ X1 + X2 + (1|group), data = temp, 
+tmodel1 <-  tmodel <- lme4::glmer(B ~ X1 + X2 + (1|group), data = temp1, 
                                   family = binomial(link = 'logit') )
-omodel <- geepack::geeglm(y ~ A + fA + X1 + A:fA, data = temp, 
+omodel1 <- geepack::geeglm(y ~ A + fA + X1 + X2 +  A:fA, data = temp1, 
                           id = group,
                           family = binomial(link = 'logit'))
 
 
-theta_t <- unlist(lme4::getME(pmodel, c('beta', 'theta')))
-theta_o <- coef(omodel)
+theta_t1 <- unlist(lme4::getME(tmodel1, c('beta', 'theta')))
+theta_o1 <- coef(omodel1)
 
 mylist <-  append(list(eeFUN = dr_eefun,
-                       splitdt = split(temp, temp$group)),
-                  list(ee_args = list(alpha = c(.5))))
+                       splitdt = split(temp1, temp1$group)),
+                  list(ee_args = list(alpha = c(.3))))
 
+fitted(omodel1)
 
-f2 <- dr_estimators(mylist$splitdt[[1]], t_model = pmodel, o_model = omodel)
-f2(c(theta_t, theta_o), alpha = .5)
 
 xx <- lapply(mylist$splitdt, function(x){
-  f <- dr_estimators(x, t_model = pmodel, o_model = omodel)
-  f(c(theta_t, theta_o), alpha = .5)
+  f <- dr_estimators(x, t_model = tmodel1, o_model = omodel1)
+  f(c(theta_t1, theta_o1), alpha = .3)
 })
 targ <- xx %>% list_matrix() %>% apply(., 2, mean)
-mats <- compute_matrices(mylist,
-                 theta   = c(theta_t, theta_o, targ),
-                 numDeriv_options = list(method = 'simple'),
-                 t_model = tmodel,
-                 o_model = omodel)
-head(mats)
-compute_sigma(mats$A, mats$B) %>% diag() %>% sqrt()
 
-summary(pmodel)
+f1 <- dr_eefun(data = mylist$splitdt[[1]], t_model = tmodel1, o_model = omodel1)
+f1(c(theta_t1, theta_o1, targ), alpha = .3) 
+
+mats <- compute_matrices(mylist,
+                 theta   = c(theta_t1, theta_o1, targ),
+                 numDeriv_options = list(method = 'simple'),
+                 t_model = tmodel1,
+                 o_model = omodel1)
+head(mats)
+compute_sigma(mats$A, mats$B) 
+
+
 # sig <- compute_sigma(mats$A, mats$B)
 # wf <- weight_estimator(A = mylist$splitdt[[1]]$A, 
 #                  X = get_design_matrix(get_fixed_formula(pmodel), mylist$splitdt[[1]]))
