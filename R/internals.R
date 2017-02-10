b7f8cc4d504af07ac8c28ef324d973c6c5394cfb
Bradley Saul <bradleysaul@gmail.com>
2017-02-10 16:45:30 -0500

    cleaning

--- R/internals.R
+++ R/internals.R
@@ -35,7 +35,8 @@ expand_grid_n <- function(n1, n2)
   if(!exists(lookup, envir = grids)){
     grids[[lookup]] <- expand.grid(ID = 1:n1, sum_a = 0:n2,
                                    A = 0:1) %>%
-      mutate_(fA = ~ sum_a / n1)
+      mutate_(fA = ~ sum_a / n1,
+              `A:fA` =~ A * fA)
   }
   return(grids[[lookup]])
 }
@@ -50,7 +51,8 @@ expand_outcome_frame <- function(X_outcome, rhs_formula_outcome){
   X_outcome %>%
     mutate_(ID = ~ row_number()) %>%
     # Remove treatment variables so that they are updated
-    select(-A, -fA) %>%
+    select(-A, -fA, -`A:fA`) %>%
+    # select(-A, -fA) %>%
     # Generate the relevant values of A and
     # all possible sum(a_i) for each subject
     full_join(expand_grid_n(n, n - 1), by = "ID")
