018c220c2039ba3b0106416510ccb15d853c2de8
Bradley Saul <bradleysaul@gmail.com>
2017-03-01 07:49:50 -0500

    initial commit of cholera analysis

---
+++ inst/cholera_analysis/2a_run_cholera_analysis.R
@@ -0,0 +1,47 @@
+#------------------------------------------------------------------------------#
+#   Title: Analysis of cholera data with IPW, OTC, and DR estimators
+#  Author: B. Saul
+#    Date: 2016-05-20
+# Purpose: 
+#------------------------------------------------------------------------------#
+
+library(dplyr)
+library(dr)
+# library(geex)
+# alphas <- c(.3, .45, .6)
+alphas <- seq(.3, .6, by = .01)
+
+
+load( pipe( 'ssh saulb@diamond.bios.unc.edu "cat /home/groups/projects/mhudgens/emch/data/R_data/emch_analysis_data.Rdata"' ))
+
+choleradt <- analysis_c %>%
+  group_by(group) %>%
+  mutate(fA = mean(A)) %>%
+  filter(n() < 1074)
+
+analysis_model_args <- list(
+  t_model = 
+    list(method = lme4::glmer,
+         formula = B ~ age + rivkm + (1|group),
+         options = list(family = binomial(link = 'logit'))),
+  o_model =
+    list(method  = geepack::geeglm,
+         # formula = y_obs ~ A + fA + A*fA + age + rivkm,
+         formula = y_obs ~ A + fA + age + rivkm,
+         options = list(
+           family  = binomial(link = 'logit'),
+           id      = quote(group)))
+)
+
+
+results <- estimate_cholera_parms(
+  data        = choleradt,
+  allocations = c(0.5),
+  model_args  = analysis_model_args
+)
+
+
+
+save(results_a0, results_a1, file = 'cholera_analysis/cholera_results.rda')
+
+  
