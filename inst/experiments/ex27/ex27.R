2dea33da76c89e894cf468a294c181d121f9e5b3
Bradley Saul <bradleysaul@gmail.com>
2017-06-03 18:18:52 -0400

    ex27 - variance of WLS estimator appears to be working

---
+++ inst/experiments/ex27/ex27.R
@@ -0,0 +1,45 @@
+#------------------------------------------------------------------------------#
+#   Title: Development ex27
+#  Author: B. Saul
+#    Date: 2017-02-25
+# Purpose: Understand and pinpoint bias observed in previous experiments
+#------------------------------------------------------------------------------#
+library(dplyr)
+library(dr)
+library(doMC)
+registerDoMC(4)
+source('inst/experiments/ex27/ex27_funs.R')
+source('inst/experiments/ex27/ex27_settings.R')
+nsims <- 100
+which_scenarios <- 9
+
+## Compute estimates ##
+allocations <- list(c(0.1, 0.5, 0.9))
+# allocations <- 0.5
+estimates <- do_scenarios(nsims, which_scenarios, allocations, 
+                          all_model_args = margs,
+                          compute_se = TRUE,
+                          verbose    = FALSE,
+                          .parallel = TRUE) %>%
+   bind_rows()
+
+estimates <- estimates %>% #select(-bias, -failed, -truth) %>%
+  group_by(model_spec, sid, method, regression_type, hajek, a, alpha) %>%
+  left_join(oracle, by = c('sid', 'a', 'alpha')) %>%
+  mutate(bias      = estimate - truth,
+         failed    = is.na(estimate) | is.infinite(estimate) | is.nan(estimate),
+         conf.low  = estimate - 1.96 * std_error,
+         conf.high = estimate + 1.96 * std_error,
+         covered   = conf.low < truth & truth < conf.high)
+
+## Compute results ## 
+results <- estimates  %>%
+  summarise(mean_est  = mean(estimate * !failed, na.rm = TRUE),
+            mean_bias = mean(bias * !failed, na.rm = TRUE),
+            coverage  = mean(covered * !failed, na.rm = TRUE),
+            failures  = sum(failed),
+            ese       = sd(estimate * !failed, na.rm = TRUE))
+
+save(estimates, scenarios, oracle, results, nsims, margs,
+     file = 'inst/experiments/ex27/ex27_results.rda')
+
